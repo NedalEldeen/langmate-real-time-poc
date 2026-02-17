f988155dadf0c75a69fa05e9c3b050cee14e90f6
Hamas Ahmed <hamasahmed0@gmail.com>
2026-02-17 23:25:24 +0200

    chore: add core folder

---
+++ apps/core/src/application/index.ts
@@ -0,0 +1,2 @@
+// Team: re-export application public API (use-cases, services, types, etc.)
+export {};
---
+++ apps/core/src/domain/index.ts
@@ -0,0 +1,2 @@
+// Team: re-export domain public API (entities, types, interfaces, etc.)
+export {};
